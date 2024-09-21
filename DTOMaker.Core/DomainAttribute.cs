6bbeead46e92c8ac7e387b47cb202c0cb854731c
Simon <simon_dudley@msn.com>
2024-09-21 16:36:49 +1000

    added Domain attribute

---
+++ DTOMaker.Core/DomainAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DTOMaker.Core
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public class DomainAttribute : Attribute
+    {
+        public DomainAttribute()
+        {
+        }
+
+        public string Version => ThisAssembly.AssemblyFileVersion;
+    }
+}
