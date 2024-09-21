1da88fd05820eb595682bd786b75e338f265d7f2
Simon <simon_dudley@msn.com>
2024-09-21 23:15:26 +1000

    codec regression tests

--- DTOMaker.Core/Codecs/Codec_Double_LE.cs
+++ DTOMaker.Core/Codecs/Codec_Double_LE.cs
@@ -7,7 +7,22 @@ namespace DTOMaker.Core.Codecs
     {
         private Codec_Double_LE() { }
         public static Codec_Double_LE Instance { get; } = new Codec_Double_LE();
-        public override double OnRead(ReadOnlySpan<byte> source) => BitConverter.Int64BitsToDouble(BinaryPrimitives.ReadInt64LittleEndian(source));
-        public override void OnWrite(Span<byte> target, in double input) => BinaryPrimitives.WriteInt64LittleEndian(target, BitConverter.DoubleToInt64Bits(input));
+        public override double OnRead(ReadOnlySpan<byte> source)
+        {
+#if NET7_0_OR_GREATER
+            return BinaryPrimitives.ReadDoubleLittleEndian(source);
+#else
+            return BitConverter.Int64BitsToDouble(BinaryPrimitives.ReadInt64LittleEndian(source));
+#endif
+        }
+
+        public override void OnWrite(Span<byte> target, in double input)
+        {
+#if NET7_0_OR_GREATER
+            BinaryPrimitives.WriteDoubleLittleEndian(target, input);
+#else
+            BinaryPrimitives.WriteInt64LittleEndian(target, BitConverter.DoubleToInt64Bits(input));
+#endif
+        }
     }
 }
