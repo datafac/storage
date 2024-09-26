d8760054e4acb20dae34290decfdde1aa9f26b0d
Psiman62 <simon_dudley@msn.com>
2024-09-26 23:06:53 +1000

    added Int32 codecs

---
+++ DTOMaker.Core/Runtime/Codec_Int32_BE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Buffers.Binary;
+
+namespace DTOMaker.Runtime
+{
+    public sealed class Codec_Int32_BE : Codec_Base<Int32>
+    {
+        private Codec_Int32_BE() { }
+        public static Codec_Int32_BE Instance { get; } = new Codec_Int32_BE();
+        public override Int32 OnRead(ReadOnlySpan<byte> source) => BinaryPrimitives.ReadInt32BigEndian(source);
+        public override void OnWrite(Span<byte> target, in Int32 input) => BinaryPrimitives.WriteInt32BigEndian(target, input);
+    }
+}
