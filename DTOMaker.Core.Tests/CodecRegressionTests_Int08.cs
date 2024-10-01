656f45a54ad2f74068ac8ee29074933319ab6397
Psiman62 <simon_dudley@msn.com>
2024-10-01 23:22:40 +1000

    added S/Byte codecs and tests

---
+++ DTOMaker.Core.Tests/CodecRegressionTests_Int08.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using System;
+using System.Linq;
+
+namespace DTOMaker.Core.Tests
+{
+    public class CodecRegressionTests_Int08
+    {
+        [Theory]
+        [InlineData((SByte)1, "01")]
+        [InlineData((SByte)0, "00")]
+        [InlineData((SByte)(-1), "FF")]
+        [InlineData(SByte.MaxValue, "7F")]
+        [InlineData(SByte.MinValue, "80")]
+        public void Roundtrip_SByte_BE(in SByte value, string expectedBytes)
+        {
+            Span<byte> buffer = stackalloc byte[1];
+            Runtime.Codec_SByte_BE.Instance.WriteTo(buffer, value);
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(expectedBytes);
+            SByte copy = Runtime.Codec_SByte_BE.Instance.ReadFrom(buffer);
+            copy.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData((SByte)1, "01")]
+        [InlineData((SByte)0, "00")]
+        [InlineData((SByte)(-1), "FF")]
+        [InlineData(SByte.MaxValue, "7F")]
+        [InlineData(SByte.MinValue, "80")]
+        public void Roundtrip_SByte_LE(in SByte value, string expectedBytes)
+        {
+            Span<byte> buffer = stackalloc byte[1];
+            Runtime.Codec_SByte_LE.Instance.WriteTo(buffer, value);
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(expectedBytes);
+            SByte copy = Runtime.Codec_SByte_LE.Instance.ReadFrom(buffer);
+            copy.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData(Byte.MinValue, "00")]
+        [InlineData((Byte)1, "01")]
+        [InlineData(Byte.MaxValue, "FF")]
+        public void Roundtrip_Byte_BE(in Byte value, string expectedBytes)
+        {
+            Span<byte> buffer = stackalloc byte[1];
+            Runtime.Codec_Byte_BE.Instance.WriteTo(buffer, value);
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(expectedBytes);
+            Byte copy = Runtime.Codec_Byte_BE.Instance.ReadFrom(buffer);
+            copy.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData(Byte.MinValue, "00")]
+        [InlineData((Byte)1, "01")]
+        [InlineData(Byte.MaxValue, "FF")]
+        public void Roundtrip_Byte_LE(in Byte value, string expectedBytes)
+        {
+            Span<byte> buffer = stackalloc byte[1];
+            Runtime.Codec_Byte_LE.Instance.WriteTo(buffer, value);
+            string.Join("-", buffer.ToArray().Select(b => b.ToString("X2"))).Should().Be(expectedBytes);
+            Byte copy = Runtime.Codec_Byte_LE.Instance.ReadFrom(buffer);
+            copy.Should().Be(value);
+        }
+
+    }
+}
--- DTOMaker.Core/Runtime/Codec_Boolean_BE.cs
+++ DTOMaker.Core/Runtime/Codec_Boolean_BE.cs
@@ -9,4 +9,32 @@ namespace DTOMaker.Runtime
         public override Boolean OnRead(ReadOnlySpan<byte> source) => source[0] != (byte)0;
         public override void OnWrite(Span<byte> target, in Boolean input) => target[0] = input ? (byte)1 : (byte)0;
     }
+    public sealed class Codec_SByte_BE : Codec_Base<SByte>
+    {
+        private Codec_SByte_BE() { }
+        public static Codec_SByte_BE Instance { get; } = new Codec_SByte_BE();
+        public override SByte OnRead(ReadOnlySpan<byte> source) => (SByte)source[0];
+        public override void OnWrite(Span<byte> target, in SByte input) => target[0] = (byte)input;
+    }
+    public sealed class Codec_SByte_LE : Codec_Base<SByte>
+    {
+        private Codec_SByte_LE() { }
+        public static Codec_SByte_LE Instance { get; } = new Codec_SByte_LE();
+        public override SByte OnRead(ReadOnlySpan<byte> source) => (SByte)source[0];
+        public override void OnWrite(Span<byte> target, in SByte input) => target[0] = (byte)input;
+    }
+    public sealed class Codec_Byte_BE : Codec_Base<Byte>
+    {
+        private Codec_Byte_BE() { }
+        public static Codec_Byte_BE Instance { get; } = new Codec_Byte_BE();
+        public override Byte OnRead(ReadOnlySpan<byte> source) => (Byte)source[0];
+        public override void OnWrite(Span<byte> target, in Byte input) => target[0] = (byte)input;
+    }
+    public sealed class Codec_Byte_LE : Codec_Base<Byte>
+    {
+        private Codec_Byte_LE() { }
+        public static Codec_Byte_LE Instance { get; } = new Codec_Byte_LE();
+        public override Byte OnRead(ReadOnlySpan<byte> source) => (Byte)source[0];
+        public override void OnWrite(Span<byte> target, in Byte input) => target[0] = (byte)input;
+    }
 }
