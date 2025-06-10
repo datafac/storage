d0add5baff1dc9d268d5d1071d1803989f00c312
Psiman62 <simon_dudley@msn.com>
2025-06-10 23:13:40 +1000

    added ITypeConverter to runtime

---
+++ DTOMaker.Runtime/ITypeConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOMaker.Runtime
+{
+    public interface ITypeConverter<TCustom, TNative>
+    {
+        TNative ToNative(TCustom custom);
+        TCustom ToCustom(TNative native);
+    }
+}
